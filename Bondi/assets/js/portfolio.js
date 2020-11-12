03df620f70d2b85440cd46fe9b086bc09ea270eb
KoTZz <32542026+KoTZz@users.noreply.github.com>
2020-11-12 15:42:20 +0200

    small portfolio update

--- Bondi/assets/js/portfolio.js
+++ Bondi/assets/js/portfolio.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
 		$(this).addClass('active');
 		$(this).siblings('li').removeClass('active');
 	})
+
+
+	$('.project a').slideUp();
+	$('.project').mouseenter(function() {
+		$(this).children('a').slideDown();
+	})
+
+	$('.project').mouseleave(function() {
+		$('.project a').slideUp();
+	})
+
 });
