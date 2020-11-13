9273317a61e3e1551c7740cee442fc033d62f1a3
KoTZz <32542026+KoTZz@users.noreply.github.com>
2020-11-13 12:33:08 +0200

    1

--- Bondi/assets/js/portfolio.js
+++ Bondi/assets/js/portfolio.js
@@ -2,8 +2,27 @@ $(document).ready(function(){
 	$('.portfolio_nav li').on('click', function() {
 		$(this).addClass('active');
 		$(this).siblings('li').removeClass('active');
-	})
 
+			if ($(this).hasClass('all')) {
+			$('.project').siblings('div').removeClass('hide');
+			}
+			if ($(this).hasClass('design')) {
+			$('.project').siblings('div').addClass('hide');
+			$('.project').siblings('.design').removeClass('hide');
+			}
+			if ($(this).hasClass('code')) {
+			$('.project').siblings('div').addClass('hide');
+			$('.project').siblings('.code').removeClass('hide');
+			}
+			if ($(this).hasClass('photos')) {
+			$('.project').siblings('div').addClass('hide');
+			$('.project').siblings('.photos').removeClass('hide');
+			}
+			if ($(this).hasClass('apps')) {
+			$('.project').siblings('div').addClass('hide');
+			$('.project').siblings('.apps').removeClass('hide');
+			}
+	})
 
 	$('.project a').slideUp();
 	$('.project').mouseenter(function() {
